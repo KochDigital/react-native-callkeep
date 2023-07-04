17da6e930c02612095b589ea94e5dd7f93f67efc
Andre Bahtiar Fauzi <supersandre@gmail.com>
2023-07-04 20:36:01 +0700

    remove binaries

---
+++ libs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+VERSION="v2.9.0"
+URL="https://github.com/aldiand/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
+LOCK=".libs.lock"
+DEST=".libs.tar.gz"
+DOWNLOAD=true
+
+if ! type "curl" > /dev/null; then
+    echo "Missed curl dependency" >&2;
+    exit 1;
+fi
+if ! type "tar" > /dev/null; then
+    echo "Missed tar dependency" >&2;
+    exit 1;
+fi
+
+if [ -f ${LOCK} ]; then
+    CURRENT_VERSION=$(cat ${LOCK})
+
+    if [ "${CURRENT_VERSION}" == "${VERSION}" ];then
+        DOWNLOAD=false
+    fi
+fi
+
+if [ "$DOWNLOAD" = true ]; then
+    curl -L --silent "${URL}" -o "${DEST}"
+    tar -xvf "${DEST}" || true
+    rm -f "${DEST}"
+
+    echo "${VERSION}" > ${LOCK}
+fi
