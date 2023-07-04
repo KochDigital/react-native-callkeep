062b29b5dfb80563ed6980a3f544714e8179c18f
Andre Bahtiar Fauzi <supersandre@gmail.com>
2023-07-04 23:44:22 +0700

    5.0.5

--- android/src/main/java/com/carusto/ReactNativePjSip/PjSipService.java
+++ android/src/main/java/com/carusto/ReactNativePjSip/PjSipService.java
@@ -63,6 +63,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import io.wazo.callkeep.R;
+
+
 public class PjSipService extends Service {
 
     private static final int FOREGROUND_SERVICE_ID = 1339;
