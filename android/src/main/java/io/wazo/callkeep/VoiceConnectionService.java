48733b4934baf0279c0e1c70ec0a97d173f6b25c
Uffe Koch <uffe@kochdigital.dk>
2024-02-23 14:09:47 +0100

    revert, as changes was in PjSipService

--- android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -71,7 +71,6 @@ import static io.wazo.callkeep.Constants.EXTRA_CALL_NUMBER_SCHEMA;
 import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 import static io.wazo.callkeep.Constants.EXTRA_DISABLE_ADD_CALL;
 import static io.wazo.callkeep.Constants.FOREGROUND_SERVICE_TYPE_MICROPHONE;
-import static io.wazo.callkeep.Constants.FOREGROUND_SERVICE_TYPE_PHONE_CALL;
 import static io.wazo.callkeep.Constants.ACTION_ON_CREATE_CONNECTION_FAILED;
 
 // @see https://github.com/kbagchiGWC/voice-quickstart-android/blob/9a2aff7fbe0d0a5ae9457b48e9ad408740dfb968/exampleConnectionService/src/main/java/com/twilio/voice/examples/connectionservice/VoiceConnectionService.java
@@ -338,11 +337,7 @@ public class VoiceConnectionService extends ConnectionService {
         Notification notification = notificationBuilder.build();
 
         try {
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) {
-                startForeground(FOREGROUND_SERVICE_TYPE_PHONE_CALL, notification);
-            } else {
-                startForeground(FOREGROUND_SERVICE_TYPE_PHONE_CALL, notification, FOREGROUND_SERVICE_TYPE_PHONE_CALL);
-            }
+            startForeground(FOREGROUND_SERVICE_TYPE_MICROPHONE, notification);
         } catch (Exception e) {
             Log.w(TAG, "[VoiceConnectionService] Can't start foreground service : " + e.toString());
         }
