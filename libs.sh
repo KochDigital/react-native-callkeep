4bb3b9ac63911bdf7ca8e09462be233339d69d95
Uffe Koch <uffe@kochdigital.dk>
2024-02-23 14:55:56 +0100

    try pjsip 3.2

--- libs.sh
+++ libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="v2.9.0"
+VERSION="v3.2"
 URL="https://github.com/aldiand/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
